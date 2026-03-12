7d442fd7edd0d36880029c8c0b02942e036c7ffa
susanthkoushik <koushik.nagasuri@gmail.com>
2026-03-12 15:41:48 +0530

    Add files via upload

---
+++ Find the sum of numbers in a given range.java
@@ -0,0 +1,12 @@
+public class Main{
+    public static void main(String[]args){
+        int i;
+        int sum =0;
+        int num1 =12;
+        int num2 =15;
+        for(i=num1;i<=num2;i++){
+            sum += i;
+            System.out.println(sum);
+        }
+    }
+}
