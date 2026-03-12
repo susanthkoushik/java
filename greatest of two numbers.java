8bee2d713000c24fff9d17aaec0b25f635392dba
susanthkoushik <koushik.nagasuri@gmail.com>
2026-03-12 16:15:10 +0530

    Add files via upload

---
+++ greatest of two numbers.java
@@ -0,0 +1,16 @@
+public class Main{
+    public static void main (String[]args){
+        int num1 = 5;
+        int num2 = 10;
+        if(num1==num2){
+            System.out.println("equal");
+        }
+        else if(num1>num2){
+            System.out.println("num1 is graeter");
+        }
+        else{
+            System.out.println("num2 is greater");
+            
+        }
+    }
+}
