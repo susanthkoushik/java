36c0b9759f7fe673ec67a9557f8f342e2c51d963
susanthkoushik <koushik.nagasuri@gmail.com>
2026-03-12 15:33:43 +0530

    Add files via upload

---
+++ Sum of first N natural number.java
@@ -0,0 +1,11 @@
+public class Main{
+    public static void main (String[]args){
+        int i ;
+        int num =6;
+        int sum =0;
+        for(i=1;i<=num;i++){
+            sum +=i;
+            System.out.println(sum);
+        }
+    }
+}
