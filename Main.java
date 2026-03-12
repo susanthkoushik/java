4affae9bfcd7e5e4c20a897d043801ce46d5d421
susanthkoushik <koushik.nagasuri@gmail.com>
2026-03-12 15:01:36 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,11 @@
+public class Main{
+    public static void main (String[]args){
+        int num = -20;
+        if(num > 0 ){
+            System.out.println("positive");
+        }
+        else{
+            System.out.println("negative");
+        }
+    }
+}
