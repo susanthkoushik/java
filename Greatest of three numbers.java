ff6e20d5a0ab07795ce67acc3e2b4f7190c3d61e
susanthkoushik <koushik.nagasuri@gmail.com>
2026-03-13 15:56:19 +0530

    Add files via upload

---
+++ Greatest of three numbers.java
@@ -0,0 +1,16 @@
+public class Main{
+    public static void main(String[]args){
+        int a=8;
+        int b=10;
+        int c=18;
+        if(a>b && a>c){
+            System .out.println("a is greater");
+        }
+        else if (b>a && b>c){
+            System.out.println("b is greater");
+        }
+        else{
+            System.out.println("c is greater");
+        }
+    }
+}
