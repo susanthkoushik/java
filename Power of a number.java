e167de93c9e98494db93f2a410a228f13725b65c
susanthkoushik <koushik.nagasuri@gmail.com>
2026-03-16 22:16:27 +0530

    Add files via upload

---
+++ Power of a number.java
@@ -0,0 +1,12 @@
+public class Main{
+    public static void main(String[]args){
+        int i;
+        int base=3;
+        int power=2;
+        int result=1;
+        for(i=1;i<=power;i++){
+            result= result*base;
+            System.out.println(result);
+        }
+    }
+}
