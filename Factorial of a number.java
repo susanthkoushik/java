7cd4d29f903b4adacffdbd5f12dd761a3e3231ae
susanthkoushik <koushik.nagasuri@gmail.com>
2026-03-17 22:46:15 +0530

    Add files via upload

---
+++ Factorial of a number.java
@@ -0,0 +1,11 @@
+public class Main{
+    public static void main(String[]args){
+        int num =5;
+        int i;
+        int result=1;
+        for(i=num;i>1;i--){
+            result=result*i;
+            System.out.println(result);
+        }
+    }
+}
