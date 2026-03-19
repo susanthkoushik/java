8041b01d91d2c60b0e981d6b622f95789c65c51e
susanthkoushik <koushik.nagasuri@gmail.com>
2026-03-19 23:13:49 +0530

    Add files via upload

---
+++ Sum of elements in array.java
@@ -0,0 +1,11 @@
+public class Main{
+    public static void main(String[]args){
+        int i;
+        int sum=0;
+        int arr[]={12,33,42,57,4};
+        for(i=0;i<arr.length;i++){
+            sum =sum+arr[i];
+            System.out.println("sum is" + " " + sum);
+        }
+    }
+}
